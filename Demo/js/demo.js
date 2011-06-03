93fc16aee4b632c5d74ff2d2e502327c9189101a
jean-philippe serafin <serafinjp@gmail.com>
2011-06-03 05:03:56 +0200

    improved demo

---
+++ Demo/js/demo.js
@@ -0,0 +1,10 @@
+window.addEvent('load', function(){
+    /* simple panorama */
+    var map = new MooPano('simple-panorama', {
+        big: 'images/big.jpg',
+    });
+    /* simple marked */
+    var map = new MooPano('marked-panorama', {
+        big: 'images/big.jpg',
+    });
+});
